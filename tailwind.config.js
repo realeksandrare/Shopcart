d9cd2c56c14ec7013adb3585148a5fd35ba0508f
Aleksandra <aleksandra.rewer@gmail.com>
2023-08-13 14:42:48 +0200

    Updated plugins in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,10 @@ module.exports = {
 			},
 		},
 	},
-	plugins: ['tailwindcss ,autoprefixer'],
+	plugins: {
+		'postcss-import': {},
+		'tailwindcss/nesting': {},
+		tailwindcss: {},
+		autoprefixer: {},
+	},
 }
